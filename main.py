2a9d33bbb0b63b078070ed2749fb2f565cddc169
tomohitonishio <150814807+tomohitonishio@users.noreply.github.com>
2024-06-06 14:57:16 +0900

    ラズパイとpixhawkの接続確認 6/6 (#2)
    Co-authored-by: tomohitonishio <nishio-tomohito048@g.ecc.u-tokyo.ac.jp>

--- main.py
+++ main.py
@@ -1,4 +1,39 @@
-def main():
-    pass
+import asyncio
+from mavsdk import System
 
-main()
+
+async def run():
+    # Create a drone object
+    drone = System()
+    
+    # Connect to the drone
+    await drone.connect(system_address="serial:///dev/serial0:57600")
+
+    # Wait for the drone to connect
+    print("Waiting for drone to connect...")
+    async for state in drone.core.connection_state():
+        if state.is_connected:
+            print(f"Connected to drone!")
+            break
+
+    # Check if drone is armable
+    print("Waiting for drone to be armable...")
+    async for is_armable in drone.telemetry.is_armable():
+        if is_armable:
+            print("Drone is armable")
+            break
+        await asyncio.sleep(1)
+
+    # Arm the drone
+    print("Arming the drone...")
+    await drone.action.arm()
+    # Wait for a few seconds before disarming
+    await asyncio.sleep(10)
+
+    # Disarm the drone
+    print("Disarming the drone...")
+    await drone.action.disarm()
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(run())
