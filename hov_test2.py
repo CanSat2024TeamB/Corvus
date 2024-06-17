2823b1aef02e5f5b8f9e0db25adfb784dbadc19b
nekochoc0 <61615276+nekochoc0@users.noreply.github.com>
2024-06-17 15:15:10 +0900

    Connection test (#16)
    * ラズパイとpixhawkの接続確認 6/6
    
    * 6/10 connection_test
    
    * 6/10
    
    * hov_new_test_6/13
    
    * hov_test_6/13
    
    * hov_test_6/13_hight
    
    * space editted
    
    * update func name
    
    * add flight func
    
    * split run func
    
    * changed func name
    
    * debug
    
    * uncommentted
    
    * debug
    
    * add test with task group
    
    ---------
    
    Co-authored-by: tomohitonishio <nishio-tomohito048@g.ecc.u-tokyo.ac.jp>
    Co-authored-by: tomohitonishio <150814807+tomohitonishio@users.noreply.github.com>
    Co-authored-by: BuildTools <unconfigured@null.spigotmc.org>

---
+++ hov_test2.py
@@ -0,0 +1,69 @@
+import asyncio
+from mavsdk import System
+
+lidar = -1
+drone = System()
+
+async def set_up():
+    global drone
+
+    # Connect to the drone
+    print("connecting now")
+    await drone.connect(system_address="serial:///dev/ttyACM0:115200")
+
+    # Wait for the drone to connect
+    print("Waiting for drone to connect...")
+    async for state in drone.core.connection_state():
+        if state.is_connected:
+            print(f"Connected to drone!")
+            break
+
+    #await drone.action.hold()
+
+    # Check if drone is armable
+
+    print("Waiting for drone to be armable...")
+    async for is_armable in drone.telemetry.health():
+        if is_armable:
+            print("Drone is armable")
+            break
+        await asyncio.sleep(1)
+
+    # Arm the drone
+    print("Arming the drone...")
+    await drone.action.arm_force()
+
+    async for is_armed in drone.telemetry.armed():
+        if is_armed:
+            print("drone is armed")
+            break
+
+async def update_lidar():
+    global lidar
+    async for distance_sensor in drone.telemetry.distance_sensor():
+        lidar = distance_sensor.current_distance_m
+
+
+async def flight():
+    global lider
+    global drone
+    
+    await drone.action.takeoff()
+
+    while lidar <= 0.3:
+        await asyncio.sleep(0.1)
+
+    await drone.action.hold()
+
+    await asyncio.sleep(5)
+
+    await drone.action.land()  
+
+async def main():
+    async with asyncio.TaskGroup() as task_group:
+        lidar_task = task_group.create_task(update_lidar())
+        flight_task = task_group.create_task(flight())
+
+if __name__ == "__main__":
+    await setup()
+    asyncio.run(main())
