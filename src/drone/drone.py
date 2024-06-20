ed9ed2f0c47e66043a0d4e9c1f1dcf22566ecc5d
BuildTools <unconfigured@null.spigotmc.org>
2024-06-20 13:10:14 +0900

    add bme680-importing code

---
+++ src/drone/drone.py
@@ -0,0 +1,20 @@
+import asyncio
+from mavsdk import System
+from sensor import lidar_handler
+from control import position_manager
+from flight import flight_controller
+
+class Drone:
+    def __init__(self):
+        self.drone = System()
+        self.set_up()        
+
+    def set_up(self) -> None:
+        self.lidar_handler = lidar_handler.LiDARHandler(self.drone)
+        self.position_manager = position_manager.PositionManager(self.drone, self.gps_handler, self.compass_handler, self.lidar_handler)
+        self.flight_controller = flight_controller.FlightController(self.drone)
+        return
+    
+    async def invoke_sensor(self) -> None:
+        async with asyncio.TaskGroup() as task_group:
+            lidar_invoke = task_group.create_task(self.lidar_handler.invoke())
