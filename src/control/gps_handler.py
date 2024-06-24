dcca05ace92ed07d28bb6417aa8cd0aa2fbddbf4
tomohitonishio <nishio-tomohito048@g.ecc.u-tokyo.ac.jp>
2024-06-24 16:40:28 +0900

    gps!

--- src/control/gps_handler.py
+++ src/control/gps_handler.py
@@ -2,16 +2,36 @@ import asyncio
 import mavsdk
 
 class GPSHandler:
-    def __init__(self,drone,Coordinates):
+    def __init__(self,drone):
         self.drone = drone
-        self.coordinates = Coordinates()
+        self.coordinates = Coordinates(None,None,None)
+        self.num_satellites: int = 0
+        self.fix_type: str = ""
+        asyncio.run(self.invoke_loop())
 
-    async def get_coordinates(self) -> None:
-        async for position in self.drone.telemetry.position():
-            self.coordinates.set_x(position.latitude_deg)
-            self.coordinates.set_y(position.longitude_deg)
-    
+
+    async def update_coordinates(self,position) -> None:
+        self.coordinates.set_x(position.longitude_deg)
+        self.coordinates.set_y(position.latitude_deg)
+        return
+
     async def invoke_loop(self) -> None:
-        while True:
-            await self.get_coordinates()
-            await asyncio.sleep(0.01)s
+        async for position in self.drone.telemetry.position():
+            self.update_coordinates(position)
+
+    #############################################################以下がオープン
+
+    async def gps_coordinates(self) -> Coordinates:
+        return self.coordinates
+    
+    async def catch_gps(self)-> None:
+        async for health in self.telemetry.health():
+            if health.is_global_position_ok and health.is_home_position_ok:
+                    break 
+                
+    async def Get_gps_info(self) -> None:
+        async for gps_info in self.telemetry.gps_info():
+            self.num_satellites = gps_info.num_satellites
+            self.fix_type = gps_info.fix_type
+
+        
