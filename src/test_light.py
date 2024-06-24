268f386f6e3b3e93bc643020f66af7b8694bfc0f
oyamada1203 <a1a1b2c3@icloud.com>
2024-06-24 19:16:19 +0900

    light

--- src/test_light.py
+++ src/test_light.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     sensor = LightSensor()
     try:
         while True:
-            value = sensor.read_value()
+            value = sensor.read_light_value()
             print(f"Light Sensor Value: {value}")
             time.sleep(1)
     except KeyboardInterrupt:
