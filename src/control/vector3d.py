fb67d1727a70d95498a19703d0991e014257a819
BuildTools <unconfigured@null.spigotmc.org>
2024-06-24 17:08:13 +0900

    change init value

--- src/control/vector3d.py
+++ src/control/vector3d.py
@@ -5,9 +5,9 @@ class Vector3d:
         self.z = z
     
     def __init__(self):
-        self.x = 0
-        self.y = 0
-        self.z = 0
+        self.x = None
+        self.y = None
+        self.z = None
     
     def x(self) -> float:
         return self.x
