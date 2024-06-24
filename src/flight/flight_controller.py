e76872c04085b64de879afc7983fc105beff339f
nekochoc0 <61615276+nekochoc0@users.noreply.github.com>
2024-06-24 19:07:29 +0900

    Flight controller (#29)
    * add flight funcs, debugged some problem
    
    * fix import relationships
    
    ---------
    
    Co-authored-by: BuildTools <unconfigured@null.spigotmc.org>

--- src/flight/flight_controller.py
+++ src/flight/flight_controller.py
@@ -1,6 +1,54 @@
+import asyncio
+from mavsdk import System
+from mavsdk.mission import (MissionItem, MissionPlan)
+
+from control.position_manager import PositionManager
+from control.coordinates import Coordinates
+
 class FlightController:
-    def __init__(self, drone_controller):
-        self.drone_controller = drone_controller
+    get_altitude_interval: float = 0.01
+
+    def __init__(self, drone: System, position_manager: PositionManager):
+        self.drone: System = drone
+        self.position_manager: position_manager
         
-    async def set_altitude(self, altitude: float):
+        self.is_in_air: bool = False
+        asyncio.run(self.invoke_loop())
+    
+    async def takeoff(self) -> bool:
+        await self.drone.action.takeoff()
+        return True
+
+    async def set_altitude(self, altitude: float) -> bool:
+        while self.position_manager.altitude() <= altitude:
+            await asyncio.sleep(get_height_interval)
+        return True
+    
+    async def go_to(self, *target_coordinates: Coordinates) -> bool:
+        #position = self.position_manager
+        mission_items = []
+        mission_items.append(MissionItem(position.latitude, position.longitude, 1, 10, True, float('nan'), float('nan'), MissionItem.CameraAction.NONE, float('nan'), float('nan'), float('nan'), float('nan'), float('nan'), MissionItem.VehicleAction.NONE))
+        for coordinates in target_coordinates:
+            mission_items.append(MissionItem(coordinates.latitude, coordinates.longitude, 1, 10, True, float('nan'), float('nan'), MissionItem.CameraAction.NONE, float('nan'), float('nan'), float('nan'), float('nan'), float('nan'), MissionItem.VehicleAction.NONE))
+        mission_plan = MissionPlan(mission_items)
+        await self.execute_mission(mission_plan)
+        return True
+
+    async def land(self) -> bool:
+        await self.drone.action.land()
+        return True
+    
+    async def execute_mission(self, mission_plan: MissionPlan) -> bool:
+        await self.drone.mission.upload_mission(mission_plan)
+        async for health in self.drone.telemetry.health():
+            if health.is_global_posision_op and health.is_home_position_ok:
+                break
+        return True
+    
+    def update_is_in_air(self, is_in_air: bool) -> None:
+        self.is_in_air = is_in_air
         return
+    
+    async def invoke_loop(self) -> None:
+        async for is_in_air in self.drone.telemetry.in_air():
+            update_is_in_air(is_in_air)
--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
 import asyncio
-from config import config_manager
-from drone import drone_controller
+from config.config_manager import ConfigManager
+from drone.drone_controller import DroneController
 
 def main():
     config_path: str = ""
-    config = config_manager.ConfigManager("assets/config/config.ini")
-    drone = drone_controller.DroneController()
+    config = ConfigManager("assets/config/config.ini")
+    drone = DroneController()
 
     asyncio.run(drone.set_up())
     return
