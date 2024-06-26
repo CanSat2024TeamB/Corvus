978c5515a834fcedf6106f028d9aea8fd0be182c
tomohitonishio <nishio-tomohito048@g.ecc.u-tokyo.ac.jp>
2024-06-27 00:44:13 +0900

    get_x

--- src/control/coordinates.py
+++ src/control/coordinates.py
@@ -1,38 +1,25 @@
 from control.vector3d import Vector3d
 
+from control.vector3d import Vector3d
+
 class Coordinates(Vector3d):
-    def __init__(self, longitude: float = None, latitude: float = None, altitude: float = None):
-        if longitude is not None and latitude is not None and altitude is not None:
-            super().__init__(longitude, latitude, altitude)
-        elif longitude is not None and latitude is not None:
-            super().__init__(longitude, latitude, 0.0)  # デフォルトで高度を0.0に設定
-        else:
-            super().__init__(0.0, 0.0, 0.0)  # デフォルトで全ての値を0.0に設定
+    def __init__(self, longitude: float = 0.0, latitude: float = 0.0, altitude: float = 0.0):
+        super().__init__(longitude, latitude, altitude)
 
     def longitude(self) -> float:
         return self.get_x()
-    
+
     def latitude(self) -> float:
         return self.get_y()
-    
+
     def altitude(self) -> float:
         return self.get_z()
-    
+
     def set_longitude(self, longitude: float) -> None:
         self.set_x(longitude)
-    
+
     def set_latitude(self, latitude: float) -> None:
         self.set_y(latitude)
-    
+
     def set_altitude(self, altitude: float) -> None:
         self.set_z(altitude)
-
-# 使用例
-coord1 = Coordinates(139.760557, 35.714995, 0)
-print(coord1.get())  # {'x': 139.760557, 'y': 35.714995, 'z': 0.0}
-
-coord2 = Coordinates(139.760557, 35.714995)
-print(coord2.get())  # {'x': 139.760557, 'y': 35.714995, 'z': 0.0}
-
-coord3 = Coordinates()
-print(coord3.get())  # {'x': 0.0, 'y': 0.0, 'z': 0.0}
--- src/control/vector3d.py
+++ src/control/vector3d.py
@@ -1,40 +1,31 @@
 class Vector3d:
-    def __init__(self, x: float, y: float, z: float):
-        self.x = x
-        self.y = y
-        self.z = z
-    
-    #def __init__(self):
-     #   self.x = 0.0
-      #  self.y = 0.0
-       # self.z = 0.0
-    
-    def x(self) -> float:
-        return self.x
-    
-    def y(self) -> float:
-        return self.y
-    
-    def z(self) -> float:
-        return self.z
-    
+    def __init__(self, x: float = 0.0, y: float = 0.0, z: float = 0.0):
+        self._x = x
+        self._y = y
+        self._z = z
+
+    def get_x(self) -> float:
+        return self._x
+
+    def get_y(self) -> float:
+        return self._y
+
+    def get_z(self) -> float:
+        return self._z
+
     def get(self) -> dict[str, float]:
-        return { "x": self.x, "y": self.y, "z": self.z }
-    
+        return {"x": self._x, "y": self._y, "z": self._z}
+
     def set_x(self, x: float):
-        self.x = x
-        return
-    
+        self._x = x
+
     def set_y(self, y: float):
-        self.y = y
-        return
-    
+        self._y = y
+
     def set_z(self, z: float):
-        self.z = z
-        return
-    
-    def set(self, x, y, z):
-        self.x = x
-        self.y = y
-        self.z = z
-        return
+        self._z = z
+
+    def set(self, x: float, y: float, z: float):
+        self._x = x
+        self._y = y
+        self._z = z
