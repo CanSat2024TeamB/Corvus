d2a87f798e2b24972e7bf2b4b871bd363308d171
tomohitonishio <nishio-tomohito048@g.ecc.u-tokyo.ac.jp>
2024-06-26 13:08:47 +0900

    utsunomiya5

--- src/sequence_test_hov.py
+++ src/sequence_test_hov.py
@@ -6,7 +6,7 @@ from drone.drone_controller import DroneController
 def main():
     #config_path: str = Path(__file__).resolve().parent.parent.joinpath("assets/config/config.ini")
     #config = ConfigManager(config_path)
-    #drone = DroneController()
+    drone = DroneController()
 
     asyncio.run(drone.set_up())
     asyncio.run(drone.invoke_sensor_and_sequence(drone.sequence_test_hovering()))
