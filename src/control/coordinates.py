cd331d5f07e0c18ee2f59bb0dac3858f7c262255
tomohitonishio <nishio-tomohito048@g.ecc.u-tokyo.ac.jp>
2024-06-27 00:30:25 +0900

    constructor

--- src/control/coordinates.py
+++ src/control/coordinates.py
@@ -1,32 +1,38 @@
 from control.vector3d import Vector3d
 
 class Coordinates(Vector3d):
-    def __init__(self, longitude: float, latitude: float, altitude: float):
-        super().__init__(longitude, latitude, altitude)
-    
-    def __init__(self, longitude: float, latitude: float):
-        super().__init__(longitude, latitude, None)
-    
-    def __init__(self):
-        super().__init__()
+    def __init__(self, longitude: float = None, latitude: float = None, altitude: float = None):
+        if longitude is not None and latitude is not None and altitude is not None:
+            super().__init__(longitude, latitude, altitude)
+        elif longitude is not None and latitude is not None:
+            super().__init__(longitude, latitude, 0.0)  # デフォルトで高度を0.0に設定
+        else:
+            super().__init__(0.0, 0.0, 0.0)  # デフォルトで全ての値を0.0に設定
 
     def longitude(self) -> float:
-        return self.x()
+        return self.get_x()
     
     def latitude(self) -> float:
-        return self.y()
+        return self.get_y()
     
     def altitude(self) -> float:
-        return self.z()
+        return self.get_z()
     
     def set_longitude(self, longitude: float) -> None:
         self.set_x(longitude)
-        return
     
     def set_latitude(self, latitude: float) -> None:
         self.set_y(latitude)
-        return
     
     def set_altitude(self, altitude: float) -> None:
         self.set_z(altitude)
-        return
+
+# 使用例
+coord1 = Coordinates(139.760557, 35.714995, 0)
+print(coord1.get())  # {'x': 139.760557, 'y': 35.714995, 'z': 0.0}
+
+coord2 = Coordinates(139.760557, 35.714995)
+print(coord2.get())  # {'x': 139.760557, 'y': 35.714995, 'z': 0.0}
+
+coord3 = Coordinates()
+print(coord3.get())  # {'x': 0.0, 'y': 0.0, 'z': 0.0}
