77bd5f9f188a827898ad77bf298551e01566fb28
tomohitonishio <nishio-tomohito048@g.ecc.u-tokyo.ac.jp>
2024-06-26 21:51:51 +0900

    main edit

--- src/sequence_test_hov.py
+++ src/sequence_test_hov.py
@@ -3,13 +3,13 @@ from pathlib import Path
 from config.config_manager import ConfigManager
 from drone.drone_controller import DroneController
 
-def main():
+async def main():
     #config_path: str = Path(__file__).resolve().parent.parent.joinpath("assets/config/config.ini")
     #config = ConfigManager(config_path)
     drone = DroneController()
 
-    asyncio.run(drone.set_up())
-    asyncio.run(drone.invoke_sensor_and_sequence(drone.sequence_test_hovering()))
+    await drone.set_up()
+    await drone.invoke_sensor_and_sequence(drone.sequence_test_hovering())
     
 
 if __name__ == "__main__":
