70f34cfc42f0bd49d336ed5dc5dfe5174a9e8b35
tomohitonishio <nishio-tomohito048@g.ecc.u-tokyo.ac.jp>
2024-06-27 01:09:57 +0900

    finished

--- src/drone/drone_controller.py
+++ src/drone/drone_controller.py
@@ -90,7 +90,8 @@ class DroneController:
         print('mission started')
         while True:
             await asyncio.sleep(0.1)
-            if await self.flight_controller.if_mission_finished():
+            mission_completed = await self.flight_controller.if_mission_finished()
+            if mission_completed:
                 print('mission finished start hovering')
                 await self.flight_controller.hovering(10)
                 print(' hovering finished start landing')
