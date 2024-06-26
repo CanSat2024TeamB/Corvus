1a1aa9ed3a79198404d21461e43ae914b2f9c13f
tomohitonishio <nishio-tomohito048@g.ecc.u-tokyo.ac.jp>
2024-06-27 00:38:04 +0900

    constructer

--- src/control/vector3d.py
+++ src/control/vector3d.py
@@ -4,10 +4,10 @@ class Vector3d:
         self.y = y
         self.z = z
     
-    def __init__(self):
-        self.x = 0.0
-        self.y = 0.0
-        self.z = 0.0
+    #def __init__(self):
+     #   self.x = 0.0
+      #  self.y = 0.0
+       # self.z = 0.0
     
     def x(self) -> float:
         return self.x
