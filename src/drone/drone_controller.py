650b4eb389dc054e7d709a6719ba78c4a8cc1626
tomohitonishio <nishio-tomohito048@g.ecc.u-tokyo.ac.jp>
2024-06-26 11:32:44 +0900

    utunomiya2

--- src/drone/drone_controller.py
+++ src/drone/drone_controller.py
@@ -72,7 +72,7 @@ class DroneController:
         await self.flight_controller.land()
         await self.flight_controller.disarm()
 
-    async def sequaence_test_mission(self,speed, *target_coordinates: Coordinates):
+    async def sequence_test_mission(self,speed, *target_coordinates: Coordinates):
         await self.flight_controller.take_off()
         await self.flight_controller.set_altitude(1.0)
         await self.flight_controller.hovering(10)
--- src/sequence_test_mission.py
+++ src/sequence_test_mission.py
@@ -1,4 +1,5 @@
 import asyncio
+from control.coordinates import Coordinates
 from pathlib import Path
 from config.config_manager import ConfigManager
 from drone.drone_controller import DroneController
@@ -8,8 +9,12 @@ def main():
     config = ConfigManager(config_path)
     drone = DroneController()
 
+    speed = 6.111
+    target_coordinates = Coordinates()
+
+
     asyncio.run(drone.set_up())
-    asyncio.run(drone.invoke_sensor_and_sequence(drone.sequence_test_hovering()))
+    asyncio.run(drone.invoke_sensor_and_sequence(drone.sequence_test_mission(speed,target_coordinates)))
     return
 
 if __name__ == "__main__":
