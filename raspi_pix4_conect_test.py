4b72c2b8ae2ae6f6dea3fba0b742d2a41e436a13
tomohitonishio <nishio-tomohito048@g.ecc.u-tokyo.ac.jp>
2024-06-29 11:44:21 +0900

    conect

---
+++ raspi_pix4_conect_test.py
@@ -0,0 +1,21 @@
+import asyncio
+from mavsdk import System
+
+drone = System()
+
+async def set_up():
+    global drone
+
+    # Connect to the drone
+    print("connecting now")
+    await drone.connect(system_address="serial:///dev/ttyACM0:115200")
+
+    # Wait for the drone to connect
+    print("Waiting for drone to connect...")
+    async for state in drone.core.connection_state():
+        if state.is_connected:
+            print(f"Connected to drone!")
+            break
+
+if __name__ == "__main__":
+    asyncio.run(set_up())
