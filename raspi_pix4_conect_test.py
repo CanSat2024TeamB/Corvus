037e3423e2dbbfea8c7cd650ffb0a32b4a1208b7
tomohitonishio <nishio-tomohito048@g.ecc.u-tokyo.ac.jp>
2024-06-29 14:48:19 +0900

    addres

--- raspi_pix4_conect_test.py
+++ raspi_pix4_conect_test.py
@@ -8,7 +8,7 @@ async def set_up():
 
     # Connect to the drone
     print("connecting now")
-    await drone.connect(system_address="serial:///dev/ttyACM0:115200")
+    await drone.connect(system_address="serial:///dev/ttyACM1:115200")
 
     # Wait for the drone to connect
     print("Waiting for drone to connect...")
