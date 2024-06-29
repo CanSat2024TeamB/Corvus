d7d4e7ddc9f80e4a0d2af079a69f1ecc3efe5367
tomohitonishio <nishio-tomohito048@g.ecc.u-tokyo.ac.jp>
2024-06-29 10:54:35 +0900

    gpio

--- src/nichrome_test.py
+++ src/nichrome_test.py
@@ -1,9 +1,9 @@
-import asyncio
-from drone.drone_controller import DroneController
+#import asyncio
+#from drone.drone_controller import DroneController
 import RPi.GPIO as GPIO
 
 def main():
-    drone = DroneController()
+    #drone = DroneController()
     #GPIO.setmode(GPIO.BCM)
     GPIO.SETUP(7,GPIO.OUT)
     GPIO.OUTPUT(7,GPIO.LOW)
