729a3ec6018cbc691d352081d48bc9ada4fd3c04
tomohitonishio <nishio-tomohito048@g.ecc.u-tokyo.ac.jp>
2024-06-29 10:39:07 +0900

    cleanup

--- src/nichrome_test.py
+++ src/nichrome_test.py
@@ -13,6 +13,9 @@ def main():
     asyncio.sleep(20)
     print('start')
     drone.para_case_stand_nichrome()
+    print('done')
+    GPIO.cleanup()
+    print('cleanup done')
 
 
 if __name__ == "__main__":
