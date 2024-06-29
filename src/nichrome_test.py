03278c825b92b17d81e29dddf099a5a75515f7dc
tomohitonishio <nishio-tomohito048@g.ecc.u-tokyo.ac.jp>
2024-06-29 10:57:21 +0900

    setmode

--- src/nichrome_test.py
+++ src/nichrome_test.py
@@ -4,7 +4,7 @@ import RPi.GPIO as GPIO
 
 def main():
     #drone = DroneController()
-    #GPIO.setmode(GPIO.BCM)
+    GPIO.setmode(GPIO.BCM)
     GPIO.setup(7,GPIO.OUT)
     GPIO.output(7,GPIO.LOW)
     print('set low')
