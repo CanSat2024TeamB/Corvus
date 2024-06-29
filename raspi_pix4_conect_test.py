d2c80a8f45fcd1e1f31bc706285333ee69602315
tomohitonishio <nishio-tomohito048@g.ecc.u-tokyo.ac.jp>
2024-06-29 17:11:05 +0900

    ama1

--- raspi_pix4_conect_test.py
+++ raspi_pix4_conect_test.py
@@ -11,8 +11,8 @@ async def set_up():
             print(port.device)
     # Connect to the drone
     print("connecting now")
-    await drone.connect(system_address="serial:///dev/ttyS0:115200")
-    #await drone.connect(system_address="serial:///dev/ttyAMA1:115200")
+    #await drone.connect(system_address="serial:///dev/ttyS0:115200")
+    await drone.connect(system_address="serial:///dev/ttyAMA1:115200")
     #await drone.connect(system_address="serial:///dev/ttyACM0:115200")
     #await drone.connect(system_address="serial:///dev/ttyACM1:115200")
 
