6a9549da3fa697b3a8af649cd16291eb0f0e137c
tomohitonishio <nishio-tomohito048@g.ecc.u-tokyo.ac.jp>
2024-06-29 11:20:20 +0900

    await

--- src/nichrome_test.py
+++ src/nichrome_test.py
@@ -2,10 +2,10 @@ import asyncio
 from drone.drone_controller import DroneController
 import RPi.GPIO as GPIO
 
-def main():
+async def main():
     drone = DroneController()
 
-    asyncio.sleep(20)
+    await asyncio.sleep(20)
     print('start')
     drone.para_case_stand_nichrome()
     print('done')
