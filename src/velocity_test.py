3251dcbbad9b2f4d3423e69d3eb53cd7b6157b7a
tomohitonishio <nishio-tomohito048@g.ecc.u-tokyo.ac.jp>
2024-06-30 16:33:22 +0900

    veltest

---
+++ src/velocity_test.py
@@ -0,0 +1,21 @@
+import asyncio
+import time
+from drone.drone_controller import DroneController
+from case.case_handler import CaseHandler
+
+
+async def main():
+    #config_path: str = Path(__file__).resolve().parent.parent.joinpath("assets/config/config.ini")
+    #config = ConfigManager(config_path)
+    dronecontroller = DroneController()
+    drone = dronecontroller.get_drone_instance()
+    case = CaseHandler(drone)
+    
+    await dronecontroller.connect()
+    await case.ac_vel.ave_velocity()
+
+
+    
+
+if __name__ == "__main__":
+    asyncio.run(main())
