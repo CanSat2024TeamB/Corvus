d005889f0cc047f9c80ad85ee406b40237ea1122
tomohitonishio <nishio-tomohito048@g.ecc.u-tokyo.ac.jp>
2024-06-30 17:07:17 +0900

    veloc

--- src/velocity_test.py
+++ src/velocity_test.py
@@ -7,18 +7,17 @@ from case.case_handler import CaseHandler
 async def main():
     #config_path: str = Path(__file__).resolve().parent.parent.joinpath("assets/config/config.ini")
     #config = ConfigManager(config_path)
-    dronecontroller = DroneController()
-    drone = dronecontroller.get_drone_instance()
+    drone_controller = DroneController()
+    drone = drone_controller.get_drone_instance()
     case = CaseHandler(drone)
     
-    await dronecontroller.connect()
-    async for imu in drone.telemetry.imu():
-            a_x = imu.acceleration_frd.forward_m_s2
-    print(a_x)
-    print("done")
-
-
+    await drone_controller.connect()
     
+    async for imu in drone.telemetry.imu():
+        a_x = imu.acceleration_frd.forward_m_s2
+        print(f"Acceleration in x-axis: {a_x}")
+    
+    print("done")
 
 if __name__ == "__main__":
     asyncio.run(main())
