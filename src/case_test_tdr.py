7e6976c2d17105ed4e25acfd9ad5c88f246516fb
tomohitonishio <nishio-tomohito048@g.ecc.u-tokyo.ac.jp>
2024-06-30 15:47:33 +0900

    system()

--- src/case_test_tdr.py
+++ src/case_test_tdr.py
@@ -8,7 +8,7 @@ from case.case_handler import CaseHandler
 async def main():
     #config_path: str = Path(__file__).resolve().parent.parent.joinpath("assets/config/config.ini")
     #config = ConfigManager(config_path)
-    drone = DroneController()
+    drone = DroneController.drone()
     case = CaseHandler(drone)
     countdown(20)
     while True:
