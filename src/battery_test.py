a1cacdefb40e7bb6c35a707c3ce2ec188f71a5d1
tomohitonishio <nishio-tomohito048@g.ecc.u-tokyo.ac.jp>
2024-07-01 16:37:11 +0900

    instance

--- src/battery_test.py
+++ src/battery_test.py
@@ -1,7 +1,13 @@
 from control.battery import Battery_watch
 import asyncio
+from drone.drone_controller import DroneController
+
 async def main():
-    battery = Battery_watch()
+    dronecontroller = DroneController()
+    drone = dronecontroller.get_drone_instance()
+    
+    await dronecontroller.connect()
+    battery = Battery_watch(drone)
     await asyncio.create_task(battery.invoke_loop())
     print(battery.voltage_v())
     print(battery.remaining_percent())
