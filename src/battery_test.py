8085aae292e0bffdd4a4dd7c464d1081269b6265
tomohitonishio <nishio-tomohito048@g.ecc.u-tokyo.ac.jp>
2024-07-01 16:53:35 +0900

    battery

--- src/battery_test.py
+++ src/battery_test.py
@@ -1,14 +1,27 @@
-from control.battery import Battery_watch
 import asyncio
+from control.battery import BatteryWatch
 from drone.drone_controller import DroneController
 
 async def main():
     dronecontroller = DroneController()
     
+    # 接続処理を非同期で実行
     await dronecontroller.connect()
-    await asyncio.create_task(dronecontroller.battery_watch.invoke_loop())
-    print(dronecontroller.battery_watch.voltage_v())
-    print(dronecontroller.battery_watch.remaining_percent())
-    print(dronecontroller.battery_watch.current_battery_a())
+    
+    # バッテリー監視タスクを開始
+    battery_task = asyncio.create_task(dronecontroller.battery_watch.invoke_loop())
+    
+    try:
+        while True:
+            # バッテリー情報を取得して表示
+            print("Voltage:", dronecontroller.battery_watch.voltage_v())
+            print("Remaining Percent:", dronecontroller.battery_watch.remaining_percent())
+            print("Current Battery A:", dronecontroller.battery_watch.current_battery_a())
+            
+            # 1秒待機
+            await asyncio.sleep(1)
+    except asyncio.CancelledError:
+        # タスクがキャンセルされた場合の処理
+        print("Battery watch task was cancelled")
 
 asyncio.run(main())
