e098c1df7d51b72d05cc960bba7d289010ac3c5b
tomohitonishio <nishio-tomohito048@g.ecc.u-tokyo.ac.jp>
2024-07-03 12:24:29 +0900

    library?

--- src/nichrome_test.py
+++ src/nichrome_test.py
@@ -1,17 +1,13 @@
+from wire.wirehandler import WireHandler
 import asyncio
-from drone.drone_controller import DroneController
-import RPi.GPIO as GPIO
-from case.case_handler import CaseHandler
 
 async def main():
     nichrome_pin_no = 23
+    nichrome_pin_duration = 5
 
-    dronecontroller = DroneController()
-    drone = dronecontroller.get_drone_instance()
-    case = CaseHandler(drone)
     await asyncio.sleep(20)
     print('start')
-    case.para_case_stand_nichrome(nichrome_pin_no)
+    WireHandler.nichrome_cut(nichrome_pin_no,nichrome_pin_duration)
     print('done')
     print('cleanup done')
 
