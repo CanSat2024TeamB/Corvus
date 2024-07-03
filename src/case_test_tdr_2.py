0cbfe31bff0ddf47daa42602bc55da033db92a23
tomohitonishio <nishio-tomohito048@g.ecc.u-tokyo.ac.jp>
2024-07-03 13:43:19 +0900

    nichrome double

--- src/case_test_tdr_2.py
+++ src/case_test_tdr_2.py
@@ -7,6 +7,7 @@ from case.case_handler import CaseHandler
 
 async def main():
     nichrome_pin_1 = 23
+    nichrome_pin_2 = 25
     #config_path: str = Path(__file__).resolve().parent.parent.joinpath("assets/config/config.ini")
     #config = ConfigManager(config_path)
     dronecontroller = DroneController()
@@ -23,9 +24,13 @@ async def main():
             print(f"Velocity not stable.restart")
 
     countdown(10)
-    print(f"nichrome cut start")
+    print(f"para and case nichrome cut start")
     case.para_case_stand_nichrome(nichrome_pin_1)
-    print(f"nichrome cut end")
+    print(f"para and case nichrome cut end")
+    countdown(10)
+    print(f"arm nichrome cut start")
+    case.para_case_stand_nichrome(nichrome_pin_2)
+    print(f"arm nichrome cut end")
     countdown(10)
     dronecontroller.arm()
 
