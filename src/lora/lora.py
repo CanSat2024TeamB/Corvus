ad675539687baa869a62336d87491f8730d2aa8d
tomohitonishio <nishio-tomohito048@g.ecc.u-tokyo.ac.jp>
2024-07-09 18:31:32 +0900

    reset_pin

--- src/lora/lora.py
+++ src/lora/lora.py
@@ -13,7 +13,7 @@ import struct
 class Lora:
     def __init__(self, drone):
         self.drone = drone
-        self.rst = 15
+        self.rst = 5
         self.md0 = 4
         self.CRLF = "\r\n"
         self.msg_received = "hello, world"
