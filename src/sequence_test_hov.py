8c86889c096142c8ff9a1d125a979d5794c15dff
tomohitonishio <nishio-tomohito048@g.ecc.u-tokyo.ac.jp>
2024-07-10 16:38:43 +0900

    task_group

--- src/sequence_test_hov.py
+++ src/sequence_test_hov.py
@@ -12,7 +12,7 @@ async def main():
     await drone.arm()
     #await drone.invoke_sensor()
     #await drone.start_sequence_task(drone.sequence_test_hovering())
-    await drone.invoke_sensor_and_sequence(drone.sequence_test_hovering)
+    await drone.invoke_sensor_and_sequence(drone.sequence_test_hovering())
     
 
 if __name__ == "__main__":
