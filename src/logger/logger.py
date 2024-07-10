b98b21d62c1cdf9e9e30d447b1e726a8adc6f4e0
tomohitonishio <nishio-tomohito048@g.ecc.u-tokyo.ac.jp>
2024-07-10 17:12:04 +0900

    log

--- src/logger/logger.py
+++ src/logger/logger.py
@@ -2,12 +2,19 @@ import os
 import datetime
 
 class Logger:
-    default_path: str = os.path.join(os.path.abspath(os.path.join(os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir)), os.pardir)), f"assets/config/log/log_{datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S')}.txt")
-
     def __init__(self):
-        self.path: str = Logger.default_path
+        # 動的にログファイルのパスを生成
+        self.path = os.path.join(
+            os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir, os.pardir)),
+            f"assets/config/log/log_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.txt"
+        )
         print(self.path)
         self.create_file(self.path)
+
+    def create_file(self, path: str):
+        # ファイルを作成し、初期メッセージを書き込みます。
+        with open(path, 'w') as file:
+            file.write("Log file created\n")
 
 
     def write(self, *msg: str) -> None:
