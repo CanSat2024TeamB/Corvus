f7c1d2a934263396960e411a78dfdbb0ec01f5ef
tomohitonishio <nishio-tomohito048@g.ecc.u-tokyo.ac.jp>
2024-07-25 18:30:54 +0900

    mission add task

--- src/sequence_test_mission.py
+++ src/sequence_test_mission.py
@@ -9,7 +9,7 @@ async def main():
     #config = ConfigManager(config_path)
     drone = DroneController()
     await drone.connect()
-    drone.invoke_sensor()
+    await drone.invoke_sensor()
     await asyncio.sleep(1)
 
     speed = 1.0
@@ -21,7 +21,7 @@ async def main():
 
 
     await drone.arm()
-    await drone.start_sequence_task(drone.sequence_test_mission(speed,target_coordinates_1,target_coordinates_2))
+    await drone.add_sequence_task(drone.sequence_test_mission(speed,target_coordinates_1,target_coordinates_2))
 
 if __name__ == "__main__":
     asyncio.run(main())
