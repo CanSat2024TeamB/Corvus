00006b911ed088d5cca39d618ae27f74900a8ef2
tomohitonishio <nishio-tomohito048@g.ecc.u-tokyo.ac.jp>
2024-07-30 15:25:48 +0900

    ttys0

--- src/lora/lora.py
+++ src/lora/lora.py
@@ -10,7 +10,7 @@ class Lora:
         self.CRLF = "\r\n"
 
         try:
-            self.serial = serial.Serial("/dev/ttyAMA0", 19200, timeout=1)
+            self.serial = serial.Serial("/dev/ttyS0", 19200, timeout=1)
         except serial.SerialException as e:
             print(f"Error opening serial port: {e}")
             raise e
