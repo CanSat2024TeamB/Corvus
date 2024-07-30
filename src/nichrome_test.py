398153235c33b7886f62fe71dd0d9569f66d6495
tomohitonishio <nishio-tomohito048@g.ecc.u-tokyo.ac.jp>
2024-07-30 18:18:02 +0900

    10

--- src/nichrome_test.py
+++ src/nichrome_test.py
@@ -4,7 +4,7 @@ import asyncio
 async def main():
     nichrome_pin_no_1 = 25
     #nichrome_pin_no_2 = 23
-    nichrome_pin_duration = 5
+    nichrome_pin_duration = 10
     wire = WireHandler()
 
     await asyncio.sleep(10)
