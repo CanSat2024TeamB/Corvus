45a6d61cadf6c1925137659c78ee4265379b5da2
tomohitonishio <nishio-tomohito048@g.ecc.u-tokyo.ac.jp>
2024-07-30 20:43:12 +0900

    power

--- src/lora/lora.py
+++ src/lora/lora.py
@@ -6,7 +6,7 @@ class Lora:
     def __init__(self, drone):
         self.drone = drone
         self.rst = 4
-        self.power = 17
+        self.power = 18
         self.CRLF = "\r\n"
         self.serial = None
 
