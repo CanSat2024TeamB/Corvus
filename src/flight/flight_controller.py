caa266a3e42c7afef83eed4346bef8052cd98fec
tomohitonishio <nishio-tomohito048@g.ecc.u-tokyo.ac.jp>
2024-08-06 13:23:41 +0900

    precise land

--- src/flight/flight_controller.py
+++ src/flight/flight_controller.py
@@ -6,21 +6,28 @@ import math
 
 from control.position_manager import PositionManager
 from control.coordinates import Coordinates
-#from sensor.camera_handler import CameraHandler, ConeDetector
+from sensor.camera_handler import CameraHandler, ConeDetector
 
 class FlightController:
 
     def __init__(self, drone: System, position_manager: PositionManager):
         self.drone: System = drone
         self.position_manager: PositionManager = position_manager
-        #self.camera_handler = CameraHandler()
-        #self.cone_detector = ConeDetector(self.camera_handler)
+        self.camera_handler = CameraHandler()
+        self.cone_detector = ConeDetector(self.camera_handler)
         self.target_latitude = 0
         self.target_longitude = 0
         self.target_altitude = 0
         self.ASML = 0
         self.yaw_deg = 0
         self.detected_pos = [None,None]
+        self.nondetected_counter = 0
+        self.nondetected_counter_max = 10
+        self.alp = 45 ## カメラ取り付け角
+        self.theta = [60, 60] ##カメラ視野角
+        self.lat_unit = 110964.027 #m 緯度一度の長さ　八千代
+        self.lon_unit = 90424.106 #m　経度一度の長さ　八千代
+
         
         self.detected_flag = False
         self.is_in_air: bool = False
@@ -105,21 +112,70 @@ class FlightController:
         while not self.if_goto_location_finished(self.target_latitude, self.target_longitude, self.target_altitude):
             await asyncio.sleep(3)
             current_alt = self.position_manager.adjusted_altitude()
-            if current_alt < 1.5:
+            if current_alt < 2:
                 await self.go_to_location(Coordinates(self.target_longitude,
                                                     self.target_latitude,
                                                     self.target_altitude + 2))
-            elif current_alt > 10:
+            elif current_alt > 8:
                 await self.go_to_location(Coordinates(self.target_longitude,
                                                     self.target_latitude,
                                                     self.target_altitude - 4))
+        self.stop_here()
         return
 
     def if_goto_location_finished(self, target_latitude, target_longitude, target_altitude):
         return abs(target_altitude - self.position_manager.adjusted_altitude()) <= 1.0 and \
-            abs(target_latitude - self.position_manager.adjusted_coordinates_lat()) <= 1.0e-5 and \
-            abs(target_longitude - self.position_manager.adjusted_coordinates_lon()) <= 1.0e-5 ## アメリカ違う
-    
+            abs(target_latitude - self.position_manager.adjusted_coordinates_lat()) *  self.lat_unit <= 2.0 and \
+            abs(target_longitude - self.position_manager.adjusted_coordinates_lon()) * self.lon_unit <= 2.0 
+
+##############################################################################################################
+
+    async def precise_land(self):
+        while self.detected_pos == [None,None]:
+                await asyncio.sleep(1)
+                self.detected_pos = self.cone_detector.capture_cone_position()
+                print(self.detected_pos)
+                self.nondetected_counter += 1
+                if self.nondetected_counter == self.nondetected_counter_max:
+                    self.go_to_location(1.0, Coordinates(self.target_longitude,self.target_latitude,self.target_altitude))
+                    self.land()
+                    return
+        
+        self.nondetected_counter = 0
+        cone_x = self.detected_pos[0]
+        cone_y = self.detected_pos[1]
+        beta =  self.theta[0] * cone_x * 0.5
+        gamma = self.theta[1] * cone_y * 0.5
+        delta = self.calculate_delta_angle(self.target_latitude,self.target_longitude) 
+        alt = self.position_manager.adjusted_altitude()
+        east_len_m = alt * math.tan(math.radians(self.alp + gamma)) * math.cos(math.radians(delta - beta))
+        north_len_m = alt * math.tan(math.radians(self.alp + gamma)) * math.sin(math.radians(delta - beta))
+        error_lon = east_len_m / self.lon_unit
+        error_lat = north_len_m / self.lat_unit
+        self.AMSL = self.position_manager.adjusted_coordinates_AMSL()
+
+        await self.drone.action.goto_location(self.target_latitude + error_lat,
+                                              self.target_longitude + error_lon, 
+                                              self.AMSL, 0)
+        await self.drone.action.set_current_speed(0.5)
+
+        while self.position_manager.adjusted_altitude() > 0.25:
+            await asyncio.sleep(0.2)
+        self.land()
+        return
+        
+
+    def calculate_delta_angle(self,target_latitude, target_longitude):
+        current_lat = self.position_manager.adjusted_coordinates_lat()
+        current_lon = self.position_manager.adjusted_coordinates_lon()
+
+        d_lat = target_latitude - current_lat
+        d_lon = target_longitude - current_lon
+
+        x = self.lon_unit * d_lon
+        y = self.lat_unit * d_lat
+        return math.degrees(math.atan2(x/y)) ##-180~180
+
     #########################################################################################################
 
     async def rotate_yaw(self, yaw):
@@ -143,23 +199,6 @@ class FlightController:
         print('descend')
         await self.drone.action.goto_location(self.target_latitude, self.target_longitude, self.AMSL+self.target_altitude-descend_m, self.yaw_deg)
 
-
-
-    async def precise_land(self):
-        while self.detected_pos == [None,None]:
-            for yaw_angle in range(0, 360, 30):
-                await self.go_to_location(yaw_angle)
-                await asyncio.sleep(3)
-                await self.stop_here()
-                self.detected_pos = self.cone_detector.capture_cone_position()
-                print(self.pos)
-        
-    
-
-                
-
-
-    #########################################################################################################
 
     async def fly_orbit(self, radius, velocity, yaw, latitude, longitude, altitude):
         await self.drone.action.do_orbit(radius_m=radius,
