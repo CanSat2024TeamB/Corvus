0e133318a665c909f272598a6b834df6ccb09f96
tomohitonishio <nishio-tomohito048@g.ecc.u-tokyo.ac.jp>
2024-08-07 14:18:59 +0900

    ytf8

--- src/lora_commandtest.py
+++ src/lora_commandtest.py
@@ -6,7 +6,7 @@ def send_and_receive(ser, data, wait_time=2):
         ser.write(data.encode('ascii'))  # データをASCIIエンコードして送信
         ser.flush()
         time.sleep(wait_time)  # 受信するための待機時間を長く設定
-        response = ser.read_all().decode('ascii').strip()  # データをASCIIデコードして受信
+        response = ser.read_all().decode('utf-8').strip()  # データをASCIIデコードして受信
         return response
     except Exception as e:
         print(f"通信エラー: {e}")
