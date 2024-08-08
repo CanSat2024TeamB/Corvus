8e6c50ad5e43a8f7b79b11efbb839a8467f84a96
oyamada1203 <a1a1b2c3@icloud.com>
2024-08-08 17:39:30 +0900

    a

--- src/sensor/pressure_handler.py
+++ src/sensor/pressure_handler.py
@@ -5,8 +5,14 @@ class PressureHandler:
     def __init__(self):
         try:
             self.sensor = bme680.BME680(bme680.I2C_ADDR_PRIMARY)
+            self.CANUSEPRESSURE = True
         except (RuntimeError, IOError):
-            self.sensor = bme680.BME680(bme680.I2C_ADDR_SECONDARY)
+            try:
+                self.sensor = bme680.BME680(bme680.I2C_ADDR_SECONDARY)
+                self.CANUSEPRESSURE = True
+            except (RuntimeError, IOError):
+                print("Error: Pressure sensor not found.")
+                self.CANUSEPRESSURE = False
         self.sensor.set_pressure_oversample(bme680.OS_4X)
         self.sensor.set_temperature_oversample(bme680.OS_8X)
         self.sensor.set_filter(bme680.FILTER_SIZE_3)
