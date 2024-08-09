b2b95cbbd6d8e86a2c721b6333cfc9464c33b57e
tomohitonishio <nishio-tomohito048@g.ecc.u-tokyo.ac.jp>
2024-08-09 13:39:54 +0900

    camera

--- src/flight/flight_controller.py
+++ src/flight/flight_controller.py
@@ -6,15 +6,15 @@ import math
 
 from control.position_manager import PositionManager
 from control.coordinates import Coordinates
-#from sensor.camera_handler import CameraHandler, ConeDetector
+from sensor.camera_handler import CameraHandler, ConeDetector
 
 class FlightController:
 
     def __init__(self, drone: System, position_manager: PositionManager):
         self.drone: System = drone
         self.position_manager: PositionManager = position_manager
-        #self.camera_handler = CameraHandler()
-        #self.cone_detector = ConeDetector(self.camera_handler)
+        self.camera_handler = CameraHandler()
+        self.cone_detector = ConeDetector(self.camera_handler)
         self.target_latitude = 0
         self.target_longitude = 0
         self.target_altitude = 0
