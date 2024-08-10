466f7a8f4170e45fcf0d24b7180b8ed9c7557257
BuildTools <unconfigured@null.spigotmc.org>
2024-08-10 12:06:55 +0900

    update

--- src/flight/flight_controller.py
+++ src/flight/flight_controller.py
@@ -16,7 +16,7 @@ class FlightController:
         self.drone: System = drone
         self.position_manager: PositionManager = position_manager
         #self.camera_handler = CameraHandler()
-        self.cone_detector = ConeDetector(self.camera_handler)
+        #self.cone_detector = ConeDetector(self.camera_handler)
         self.target_latitude = 0
         self.target_longitude = 0
         self.target_altitude = 0
