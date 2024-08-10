9a7a7db892ec6522fcdfa028b008a7b10c28b8ea
BuildTools <unconfigured@null.spigotmc.org>
2024-08-10 14:45:21 +0900

    capture_cone_position_and_saveを変更

--- src/sensor/camera_handler.py
+++ src/sensor/camera_handler.py
@@ -120,9 +120,8 @@ class ConeDetector:
 
         if not image is None:
             result = cone_detector.get_pos(image, conf)
-            cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
             if result[0] >= -1:
-                cv2.circle(image, (int((result[0] + 1) / 2 * 640), int(-(result[1] + 1) / 2 * 480)), 25, (255, 0, 0), 5)
+                image = cv2.circle(image, (int((result[0] + 1) / 2 * 640), int((-result[1] + 1) / 2 * 480)), 25, (255, 255, 255), tickness = -1)
             cv2.imwrite(output_path, image)
             return result
         else:
