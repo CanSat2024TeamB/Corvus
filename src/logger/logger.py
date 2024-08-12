04fb59c1a877e12c203fa0967a3ce6555e1f8296
BuildTools <unconfigured@null.spigotmc.org>
2024-08-13 07:57:31 +0900

    loggerを更新

--- src/logger/logger.py
+++ src/logger/logger.py
@@ -11,15 +11,16 @@ class Logger:
         print(f"Logs will be written to the file, {self.path}")
         self.create_file(self.path)
 
-    def create_file(self, path: str):
+    def create_file(self) -> bool:
         # ディレクトリが存在しない場合は作成する
-        os.makedirs(os.path.dirname(path), exist_ok=True)
+        os.makedirs(os.path.dirname(self.dir), exist_ok=True)
         # ファイルを作成し、初期メッセージを書き込みます。
-        with open(path, 'w') as file:
-            file.write("Log file created\n")
+        self.write("Log file created")
+        return True
 
-    def write(self, *msg: str) -> None:
+    def write(self, *msg: str) -> bool:
         timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         with open(self.path, 'a', encoding="UTF-8") as f:
             f.write(f"{timestamp} {' '.join(msg)}\n")
+        return True
 
