22cb29c9b6969d5eafee902d1efa676e5ce9cf30
BuildTools <unconfigured@null.spigotmc.org>
2024-08-13 17:08:01 +0900

    offboard_precise_landingの降下速度を変更

--- src/flight/flight_controller.py
+++ src/flight/flight_controller.py
@@ -188,7 +188,7 @@ class FlightController:
         CAMERA_YAW_DEG = 0 #pixhawk正面からはかったカメラの指向方向 (deg, 右回り正)
         LAND_ALTITUDE = 0.25 #コーンに接近していってlandに移行する高度
         PROB_THRESHOLD = 0.2 #画像認識probabilityの閾値
-        DECENDING_SPEED = 0.1 #降下速度（2^0.5を乗じた値が降下速度）
+        DECENDING_SPEED = 1 #降下速度（2^0.5を乗じた値が降下速度）
         ADJUST_FACTOR = 0.1 #上下左右方向の補正係数
 
         if not self.camera_handler.is_connected():
