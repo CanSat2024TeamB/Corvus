a93d29814ab59f5ecd46904f7f8eeeb1b0c951d5
BuildTools <unconfigured@null.spigotmc.org>
2024-08-13 09:15:29 +0900

    ファイルのディレクトリ依存を修正

--- src/sensor/camera_handler.py
+++ src/sensor/camera_handler.py
@@ -1,7 +1,7 @@
 import sys
 from pathlib import Path
 
-sys.path.append(Path(__file__).parent.parent.joinpath("assets/module"))
+sys.path.append(str(Path(__file__).parent.parent.joinpath("assets/module")))
 
 from pathlib import Path
 from picamera2 import Picamera2
