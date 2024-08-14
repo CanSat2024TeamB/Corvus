ed87e53a6df316754df66e76aec7bd8d3affaa69
BuildTools <unconfigured@null.spigotmc.org>
2024-08-15 08:26:00 +0900

    noshiro_main.pyの座標値を変更

--- src/noshiro_main.py
+++ src/noshiro_main.py
@@ -48,7 +48,7 @@ async def main():
     first_lat = drone.position_manager.adjusted_coordinates_lat()
     hov_alt = 5
     target_coordinates_1 = Coordinates(first_lon,first_lat,hov_alt)
-    target_coordinates_2 = Coordinates(140.1080417,35.7702389,5)
+    target_coordinates_2 = Coordinates(140.05860198099998, 40.193250305, 5)
     
     await drone.add_sequence_task(drone.sequence_test_mission(speed,target_coordinates_1,target_coordinates_2))
     try:
