dfbfe2ea4d5bc4e4fdf88d6d77776306f2a0a202
BuildTools <unconfigured@null.spigotmc.org>
2024-08-14 18:17:43 +0900

    delete config_test.py

--- src/config_test.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-from config.config_manager import ConfigManager
-
-config = ConfigManager()
-
-speed = config.read_float("NOSHIRO", "Speed")
-print(speed)
-print(type(speed))
