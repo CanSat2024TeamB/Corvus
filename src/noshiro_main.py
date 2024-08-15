621483e8f3bb0d460a9d5ab84de1b149bf0d67dd
tomohitonishio <nishio-tomohito048@g.ecc.u-tokyo.ac.jp>
2024-08-15 18:14:10 +0900

    case

--- src/noshiro_main.py
+++ src/noshiro_main.py
@@ -50,7 +50,7 @@ async def main():
     logger.write(f"1para and case nichrome cut end")
 
     case.nichrome_cleanup()
-    await asyncio.sleep(120)
+    await asyncio.sleep(60)
 
     #config_path: str = Path(__file__).resolve().parent.parent.joinpath("assets/config/config.ini")
     #config = ConfigManager(config_path)
