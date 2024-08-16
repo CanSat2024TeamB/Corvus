9d4090de32e2841757a632438df758fecd137296
tomohitonishio <nishio-tomohito048@g.ecc.u-tokyo.ac.jp>
2024-08-16 12:51:45 +0900

    last

--- src/noshiro_main.py
+++ src/noshiro_main.py
@@ -69,7 +69,7 @@ async def main():
     #hov_alt = 5
     #target_coordinates_1 = Coordinates(first_lon,first_lat,hov_alt)
     #target_coordinates_2 = Coordinates(139.987196935, 40.142438475, 5) honnbann
-    target_coordinates_2 = Coordinates(140.05863601,40.193261534,5)
+    target_coordinates_2 = Coordinates(139.987197098,40.142462332,5)
     
     
     await drone.add_sequence_task(drone.sequence_test_goto(speed, target_coordinates_2))
