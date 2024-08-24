e83cfdc8a7776d935a0b61451beea12d391b087a
tomohitonishio <nishio-tomohito048@g.ecc.u-tokyo.ac.jp>
2024-08-24 14:20:53 +0900

    nichrome

--- src/test/nichrome_test.py
+++ src/test/nichrome_test.py
@@ -8,15 +8,15 @@ import asyncio
 
 async def main():
     nichrome_pin_no_1 = 25
-    #nichrome_pin_no_2 = 23
+    nichrome_pin_no_2 = 23
     nichrome_pin_duration = 10
     wire = WireHandler()
 
     await asyncio.sleep(10)
     print('start')
-    wire.nichrome_cut(nichrome_pin_no_1,nichrome_pin_duration)
+    wire.nichrome_cut(nichrome_pin_no_2,nichrome_pin_duration)
     await asyncio.sleep(5)
-    wire.nichrome_cut(nichrome_pin_no_1,nichrome_pin_duration)
+    wire.nichrome_cut(nichrome_pin_no_2,nichrome_pin_duration)
     #print('start')
     #wire.nichrome_cut(nichrome_pin_no_2,nichrome_pin_duration)
     #print('done')
