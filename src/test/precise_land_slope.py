b0c41b9a2182b40c9fecba2806c70725b6041bba
tomohitonishio <nishio-tomohito048@g.ecc.u-tokyo.ac.jp>
2024-08-27 09:16:08 +0900

    precise_land

---
+++ src/test/precise_land_slope.py
@@ -0,0 +1,37 @@
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).parent.parent))
+
+import asyncio
+from control.coordinates import Coordinates
+from pathlib import Path
+from config.config_manager import ConfigManager
+from drone.drone_controller import DroneController
+
+async def main():
+    # config_path: str = Path(__file__).resolve().parent.parent.joinpath("assets/config/config.ini")
+    # config = ConfigManager(config_path)
+    drone = DroneController()
+    await drone.connect()
+    
+    # `invoke_sensor`の呼び出し
+    asyncio.create_task(drone.invoke_sensor())
+
+    # すぐに`invoke_sensor`タスクを開始する
+    await asyncio.sleep(1)
+
+    speed = 1.0
+    target_coordinates = Coordinates(139.887499386, 35.766242252, 11.156080307960511)
+
+    # `add_sequence_task`の呼び出し
+    await drone.add_sequence_task(drone.sequence_test_goto_and_precise_land_slope(speed, target_coordinates))
+
+    try:
+        # 無限ループを維持するためのFutureを作成
+        await asyncio.Future()
+    except asyncio.CancelledError:
+        print("Main loop cancelled")
+
+if __name__ == "__main__":
+    asyncio.run(main())
