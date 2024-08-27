91819cddcc1fb0720418abcef833f69d2561edea
tomohitonishio <nishio-tomohito048@g.ecc.u-tokyo.ac.jp>
2024-08-27 09:48:18 +0900

    preciseland

--- src/flight/flight_controller.py
+++ src/flight/flight_controller.py
@@ -32,8 +32,8 @@ class FlightController:
         self.nondetected_counter_max = 10
         self.alp = 45 ## カメラ取り付け角
         self.theta = [54, 41] ##カメラ視野角
-        self.lat_unit = 111047.008 #m 緯度一度の長さ　八千代
-        self.lon_unit = 85215.711 #m　経度一度の長さ　八千代
+        self.lat_unit = 110945.467 #m 緯度一度の長さ　八千代
+        self.lon_unit = 90428.693 #m　経度一度の長さ　八千代
 
         
         self.detected_flag = False
@@ -152,7 +152,7 @@ class FlightController:
             await asyncio.sleep(1)
             self.current_lidar_alt = self.position_manager.adjusted_altitude()
             
-            if self.current_lidar_alt < 5:
+            if self.current_lidar_alt < 3:
                 print('altitude too low')
                 self.target_final_altitude += 0.1
                 print('target AMSL alt',self.target_final_altitude)
