4aa79bac347b184c81ea6c35c9c54a618813315e
tomohitonishio <nishio-tomohito048@g.ecc.u-tokyo.ac.jp>
2024-08-29 11:09:44 +0900

    video output path

--- src/test/video_test.py
+++ src/test/video_test.py
@@ -8,8 +8,10 @@ import time
 from sensor.camera_handler import CameraHandler
 
 camera_handler = CameraHandler.get_instance()
+output_path = str(Path(__file__).parent.parent.parent.joinpath(f"assets/log/mov_{datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S')}.mp4"))
+
 
 print("start capturing")
-camera_handler.capture_video("test.mp4", 10)
+camera_handler.capture_video(output_path, 10)
 time.sleep(15)
 print("finish")
