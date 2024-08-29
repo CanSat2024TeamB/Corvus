56f8cdf3454e0c2c7b064e428e53965bdc5d5b43
tomohitonishio <nishio-tomohito048@g.ecc.u-tokyo.ac.jp>
2024-08-29 11:47:06 +0900

    python3

--- src/flight/flight_controller.py
+++ src/flight/flight_controller.py
@@ -478,7 +478,7 @@ class FlightController:
         
         count = 0
         pos = [None, None]
-        while count < 10 & pos[0] == None:
+        while count < 10 and pos[0] is None:  # `&` を `and` に修正
             count += 1
             image = self.camera_handler.capture_bgr()
             pos = self.cone_detector.calc_color_center(image)
