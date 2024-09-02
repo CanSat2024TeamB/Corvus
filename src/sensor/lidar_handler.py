25c83ceb21df30be3daa8251f0558d937d597265
BuildTools <unconfigured@null.spigotmc.org>
2024-09-02 17:53:16 +0900

    lidarの更新を確かめる

--- src/sensor/lidar_handler.py
+++ src/sensor/lidar_handler.py
@@ -6,8 +6,8 @@ class LiDARHandler:
         self.altitude: float = -1.0
 
     def update_altitude(self, altitude: float) -> None:
-        print("lidar updated")
-        self.drone.get_logger_instance().write("lidar updated")
+        print("lidar updated", altitude)
+        self.drone.get_logger_instance().write("lidar updated", altitude)
         self.altitude = altitude
         return
 ###################################################以下オープンにする    
