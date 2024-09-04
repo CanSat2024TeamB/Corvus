4b6e88734b7cfae914a0fc24ae115b2e60b28fcc
BuildTools <unconfigured@null.spigotmc.org>
2024-09-04 17:18:11 +0900

    画像認識をマルチプロセス化

--- src/sensor/camera_handler.py
+++ src/sensor/camera_handler.py
@@ -13,6 +13,8 @@ import threading
 import cone_detector
 import time
 
+import multiprocessing
+
 class CameraHandler:
     _unique_instance = None
 
@@ -89,7 +91,6 @@ class CameraHandler:
 
 class ConeDetector:
     IOU_THRESHOLD = 0.1
-    condition = threading.Condition()
 
     def __init__(self, camera_handler, model_path: str = Path(__file__).parent.parent.parent.joinpath("assets/model/cone_ncnn_model_v9_320_opt"), imgsz = 320):
         self.camera_handler: CameraHandler = camera_handler
@@ -101,14 +102,10 @@ class ConeDetector:
         self.finished = False
     
     def get_pos(self, use_color_assist = False):
-        self.condition.acquire()
         pos = self.pos
-        self.condition.release()
 
         if pos[0] is None and use_color_assist: # 機械学習による推論で物体推定ができなかった場合色情報をもとに推定を行う
-            self.condition.acquire()
             frame = self.frame
-            self.condition.release()
 
             color_pos = self.calc_color_center(frame)
             if color_pos[0] is not None:
@@ -147,46 +144,25 @@ class ConeDetector:
             x = result["m10"] / result["m00"]
             y = result["m01"] / result["m00"]
             return [x / width * 2 - 1, 1 - y / height * 2]
-
-    
-    def reader(self):
-        while not self.finished:
-            try:
-                frame = self.camera_handler.capture_bgr()
-            except Exception as e:
-                print(e)
-                frame = np.zeros((self.camera_handler.get_height, self.camera_handler.get_width, 3))
-            self.condition.acquire()
-            self.frame = frame
-            self.condition.notify()
-            self.condition.release()
     
     def detector(self, conf = IOU_THRESHOLD):
         while not self.finished:
-            self.condition.acquire()
-            if self.frame is None:
-                self.condition.wait()
-            frame = self.frame
-            self.condition.release()
-
+            print("capturing...")
+            frame = self.camera_handler.capture_bgr()
+            print("captured")
             pos = cone_detector.get_pos(frame, conf)
-            
-            self.condition.acquire()
             if pos[0] < -1:
                 self.pos = [None, None]
             else:
                 self.pos = pos
-            self.condition.release()
 
     def start(self, conf = IOU_THRESHOLD):
         if self.started:
             return
         
-        reader_thread = threading.Thread(target = self.reader, daemon = True)
-        detector_thread = threading.Thread(target = self.detector, args = (conf,), daemon = True)
-
-        reader_thread.start()
-        detector_thread.start()
+        process = multiprocessing.Process(target = self.detector, args = (conf,), daemon = True)
+        process.start()
+        self.started = True
     
     def stop(self):
         self.finished = True
