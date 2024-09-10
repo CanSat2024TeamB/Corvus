fa813397efd6bca38f863d9001ab986f25902774
BuildTools <unconfigured@null.spigotmc.org>
2024-09-10 06:55:27 -0700

    定数を変更

--- src/flight/flight_controller.py
+++ src/flight/flight_controller.py
@@ -463,7 +463,7 @@ class FlightController:
         CAMERA_YAW_DEG = 0 #pixhawk正面からはかったカメラの指向方向 (deg, 右回り正)
         LAND_ALTITUDE = 0.5 #コーンに接近していってlandに移行する高度
         PROB_THRESHOLD = 0.2 #画像認識probabilityの閾値
-        DECENDING_SPEED = 0.5 #降下速度
+        DECENDING_SPEED = 0.2 #降下速度
         ADJUST_FACTOR = 1.5 #上下左右方向の補正係数(1.0が無調整)
 
         arr = multiprocessing.Array("f", 2)
@@ -524,9 +524,9 @@ class FlightController:
         def calc_velocity_body_to_target(self, pos) -> VelocityBodyYawspeed:
             nonlocal CAMERA_YAW_DEG
             nonlocal ADJUST_FACTOR
-            front_vec = math.sin(math.radians(45 + pos[1] * self.theta[1] / 2 * ADJUST_FACTOR * 1.3))
+            front_vec = math.sin(math.radians(45 + pos[1] * self.theta[1] / 2 * ADJUST_FACTOR * 1.1))
             right_vec = math.sin(math.radians(pos[0] * self.theta[0] / 2 * ADJUST_FACTOR))
-            down_vec = math.cos(math.radians(pos[0] * self.theta[0] / 2 * ADJUST_FACTOR)) * math.cos(math.radians(45 + pos[1] * self.theta[1] / 2 * ADJUST_FACTOR * 1.3))
+            down_vec = math.cos(math.radians(pos[0] * self.theta[0] / 2 * ADJUST_FACTOR)) * math.cos(math.radians(45 + pos[1] * self.theta[1] / 2 * ADJUST_FACTOR * 1.1))
             #front_vec = math.sin(math.radians(45 + pos[1] * self.theta[1] / 2)) * math.cos(math.radians(CAMERA_YAW_DEG + pos[0] * self.theta[0] / 2))
             #right_vec = math.sin(math.radians(45 + pos[1] * self.theta[1] / 2)) * math.sin(math.radians(CAMERA_YAW_DEG + pos[0] * self.theta[0] / 2))
             #down_vec = math.cos(math.radians(45 + pos[1] * self.theta[1] / 2))
