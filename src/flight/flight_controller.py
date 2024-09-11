ed70423f1ef373193adb9bc201dfe3d463cc6d01
BuildTools <unconfigured@null.spigotmc.org>
2024-09-11 13:55:57 -0700

    パラメータを調整

--- src/flight/flight_controller.py
+++ src/flight/flight_controller.py
@@ -464,9 +464,9 @@ class FlightController:
     async def offboard_precise_land(self) -> bool:        
         CAMERA_YAW_DEG = 0 #pixhawk正面からはかったカメラの指向方向 (deg, 右回り正)
         LAND_ALTITUDE = 0.2 #コーンに接近していってlandに移行する高度
-        PROB_THRESHOLD = 0.2 #画像認識probabilityの閾値
+        PROB_THRESHOLD = 0.3 #画像認識probabilityの閾値
         DECENDING_SPEED = 0.5 #降下速度
-        ADJUST_FACTOR = 1.8 #上下左右方向の補正係数(1.0が無調整)
+        ADJUST_FACTOR = 1.7 #上下左右方向の補正係数(1.0が無調整)
 
         arr = multiprocessing.Array("f", 2)
         arr[0] = -2
