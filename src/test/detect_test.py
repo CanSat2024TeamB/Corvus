6254391129d5c581f868853e0d175ec554eb43db
BuildTools <unconfigured@null.spigotmc.org>
2024-09-12 04:47:10 -0700

    テストコードを追加

--- src/test/detect_test.py
+++ src/test/detect_test.py
@@ -7,7 +7,7 @@ from pathlib import Path
 import cv2
 import numpy as np
 import os
-import threading
+import multiprocessing
 import cone_detector
 import time
 
@@ -17,17 +17,24 @@ def detect(image, conf, output_path):
     pos = cone_detector.get_pos(image, conf)
     height = image.shape[0]
     width = image.shape[1]
+    result = image
     if pos[0] >= -1:
         x = int((pos[0] + 1) / 2 * width)
         y = int((pos[1] + 1) / 2 * height)
         result = cv2.circle(image, (x, y), 25, (0, 0, 255), thickness = 5)
-        cv2.imwrite(output_path, result)
+    cv2.imwrite(output_path, result)
 
-cone_detector.load_model(str(Path(__file__).parent.parent.parent.joinpath("assets/model/cone_ncnn_model_v9_320_opt")), 320)
-in_path = "/home/admin/imgs/"
-out_path = "/home/admin/result/"
-imgs = os.listdir(in_path)
-for img in imgs:
-    img_path = in_path + img
-    image = cv2.imread(img_path)
-    detect(image, 0.3, out_path + img)
+def main(conf):
+    cone_detector.load_model(str(Path(__file__).parent.parent.parent.joinpath("assets/model/cone_ncnn_model_v9_320_opt")), 320)
+    in_path = "/home/admin/imgs/"
+    out_path = f"/home/admin/result{conf}/"
+    imgs = os.listdir(in_path)
+    for img in imgs:
+        img_path = in_path + img
+        image = cv2.imread(img_path)
+        detect(image, conf, out_path + img)
+        time.sleep(0.3)
+
+proc_1 = multiprocessing.Process(target=main, args=(0.6,))
+proc_2 = multiprocessing.Process(target=main, args=(0.7,))
+proc_3 = multiprocessing.Process(target=main, args=(0.8,))
